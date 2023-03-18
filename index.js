c80f07b32f142805247626f0eebf40ce266dda5a
Tayler Schanks <taylerschanks2@gmail.com>
2023-03-18 13:47:12 -0500

    Phase 1 functions lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+let hQ = 42;
+function distanceFromHqInBlocks(distance1) {
+    return Math.abs(distance1 - hQ);
+}
+
+function distanceFromHqInFeet(distance2) {
+    return Math.abs(distance2 - hQ) * 264;
+}
+
+function distanceTravelledInFeet(distance3, distance4) {
+    return Math.abs(distance3 - distance4) * 264;
+}
+
+function calculatesFarePrice(startLocation, endLocation) {
+    if (Math.abs(startLocation - endLocation) * 264 <= 400) {
+        return 0;
+    } else if (Math.abs(startLocation - endLocation) * 264 > 400 && (Math.abs(startLocation - endLocation) * 264) <= 2000) {
+        return ((Math.abs(startLocation - endLocation) * 264) - 400) * 0.02;
+    } else if (Math.abs(startLocation - endLocation) * 264 > 2000 && (Math.abs(startLocation - endLocation) * 264) <= 2500) {
+        return 25;
+    } else if (Math.abs(startLocation - endLocation) * 264 > 2500) {
+        return "cannot travel that far";
+    }
+}
